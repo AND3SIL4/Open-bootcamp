dfc93d4008d26760ed7439e3a387363d99344386
AND3FELIPE <afsilva6236@misena.edu.co>
2023-06-13 14:19:49 -0500

    Ejercicio 1 | inicio con Node

---
+++ curso de js/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,7 @@
+// Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al proyecto");
+
+
+
+// Este es el mesaje de despedida
+console.log("Esta es la puerta de salida del proyecto");
