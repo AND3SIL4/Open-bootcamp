270f67a3e2b3cb34900bd51374b6385d42800ae8
AND3FELIPE <afsilva6236@misena.edu.co>
2023-06-23 11:24:19 -0500

    Ejercicio 4 | Strings

--- curso de js/ejercicio-3/loops.js
+++ curso de js/ejercicio-3/loops.js
@@ -1,21 +1,23 @@
-// factorial-for.js
-let factorial = 1;
-for (let num = 2; num <= 10; num++) {
-    factorial *= num;
-}
-console.log("El factorial de 10 es:", factorial, "con for");
+// // factorial-for.js
+// let factorial = 1;
+// for (let num = 2; num <= 10; num++) {
+//     factorial *= num;
+// }
+// console.log("El factorial de 10 es:", factorial, "con for");
 
-// // factorial-while.js
-let factorial = 1;
-let num = 2;
+// // // factorial-while.js
+// let factorial = 1;
+// let num = 2;
 
-while (num <= 10) {
-  factorial *= num
-  num++;
-}
-console.log("El factorial de 10 es: ", factorial, "con while");
+// while (num <= 10) {
+//   factorial *= num
+//   num++;
+// }
+// console.log("El factorial de 10 es: ", factorial, "con while");
 
-// factorial-break.js 
+// factorial-break.js
+
+let nombre = 'Andres Silva'
 let factorial = 1;
 let num = 1;
 BucleWhile: while (true) {
@@ -25,4 +27,4 @@ BucleWhile: while (true) {
   factorial *= num;
   num++
 }
-console.log(factorial);
+console.log(`Hola ${factorial}`); 
