d9b1cfccc9df23f5747f22a1578d905cf8928330
AND3FELIPE <afsilva6236@misena.edu.co>
2023-06-27 11:08:40 -0500

    New(ejercicio-13/index.js) : Ejercicio 13 - Trabajando con ESlint

---
+++ curso de js/ejercicio-13/index.js
@@ -0,0 +1,7 @@
+let comillas_simples = 'Esto es un ejemplo de comillas simples'
+let comillas_dobles = 'Esto es un ejemplo de comillas dobles'
+/*eslint-disable */
+let backsticks = `Esto es un ejemplo de backsticks` 
+/*eslint-enable */
+
+console.log(comillas_simples, comillas_dobles, backsticks)
